2490cd0cf4acd33c36f17e79f1bb04f2691722c1
Antoine Chauvin <antoine.chauvin@live.fr>
2022-02-04 16:31:32 +0100

    Update 2.py

--- 2.py
+++ 2.py
@@ -6,16 +6,9 @@ __version__ = "1.0"
 __maintainer__ = "Chauvin Antoine"
 __email__ = "antoine.chauvin@live.fr"
 __status__ = "Production"
-# On importe la librairie asyncio
 import asyncio
-# On importe la librairie concurrent.futures pour executer de manière asynchrone une tâche dite bloquante
 import concurrent.futures
 import logging
-#On utilise la journalisation pour indiquer de manière
-# pratique quel thread et quelle fonction produisent chaque message de journal.
-# Comme un enregistreur distinct est utilisé dans chaque appel à blocks(),
-# la sortie montre clairement que les mêmes threads sont réutilisés pour appeler plusieurs copies
-# de la fonction avec différents arguments.
 import sys
 import time
 
@@ -27,45 +20,32 @@ def blocks(n):
     log.info('done')
     return n ** 2
 
-# Explicit
-async def run_blocking_tasks(executor):
-    log = logging.getLogger('run_blocking_tasks')
+async def main():
+    log = logging.getLogger('main')
     log.info('starting')
+
+    loop = asyncio.get_running_loop()
+
+    # Create a limited thread pool.
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    loop.set_default_executor(executor)
+
     log.info('creating executor tasks')
-    
-    # On récupère la boucle dévénement
-    loop = asyncio.get_event_loop()
-    # On spécifie ici lesfonctions à executer sous forme de liste avec les paramètres
-    blocking_tasks = [
-        loop.run_in_executor(executor, blocks, i)
-        for i in range(6)
-    ]
+    awt = [asyncio.to_thread(blocks, 3),
+    asyncio.to_thread(blocks, 3),
+    asyncio.to_thread(blocks, 3),
+    asyncio.to_thread(blocks, 3),
+    asyncio.to_thread(blocks, 3),
+    asyncio.to_thread(blocks, 3)]
+
     log.info('waiting for executor tasks')
+    await asyncio.gather(*awt)
     
-    # On redonne la main pour attendre que chaque tâche se finisse
-    completed, pending = await asyncio.wait(blocking_tasks)
-    
-    results = [t.result() for t in completed]
-    log.info('results: {!r}'.format(results))
     log.info('exiting')
 
 
 if __name__ == '__main__':
-  # On configure une journalisation basique afin d'afficher
-  # le nom du Thread et son message.
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(threadName)10s %(name)18s: %(message)s',
-        stream=sys.stderr,
-    )
-
-    # On va créer un PoolExecutor qui contiendra des Threads
-    # qu'on va limiter volontairement à 3 workers
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-    try:
-        # On créer notre boucle d'événement dans laquel on renseigne la fonction à executer
-        event_loop.run_until_complete(
-            run_blocking_tasks(executor)
-        )
-    finally:
-        event_loop.close()
+    # Configure logging to show the name of the thread
+    # where the log message originates.
+    logging.basicConfig(level=logging.INFO, format='%(threadName)10s %(name)18s: %(message)s', stream=sys.stderr)
+    asyncio.run(main())
